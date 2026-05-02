4053b79e2437db7cb29371e63ca4141b28664222
KathyKGB <150852234+KathyKGB@users.noreply.github.com>
2026-05-01 23:42:34 -0400

    Update AppNavigation.kt

--- android/app/src/main/java/derot/ui /AppNavigation.kt
+++ android/app/src/main/java/derot/ui /AppNavigation.kt
@@ -1,1 +1,21 @@
+package com.rork.freshguard.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rork.freshguard.ui.screens.HomeScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+    }
+}
